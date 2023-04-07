a771a6c8d6ad20178fbb098ee5455f21424164b5
Leonardo Rodrigues <leonardoacrg@gmail.com>
2023-04-07 11:39:47 -0300

    Backend: Deploy on EC2

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -8,10 +8,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-htgbmz+=*4f#+7can0c#m=owdplmp+swoj-1j9y%q#+#l=epr%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
