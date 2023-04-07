0945f444860261c296f9c5c073ff5e07515be87f
Leonardo Rodrigues <leonardoacrg@gmail.com>
2023-04-07 12:12:09 -0300

    Frontend: Deploy on Vercel

--- frontend/src/services/api-get-request.ts
+++ frontend/src/services/api-get-request.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const urlBase = "http://127.0.0.1:8000/api/";
+// const urlBase = "http://127.0.0.1:8000/api/";
+const urlBase = "http://54.205.57.183:8000/api/";
 
 export const getDashboardData = async () => {
     const response = await axios.get(`${urlBase}dashboard-data/`);
--- frontend/src/services/api-post-request.ts
+++ frontend/src/services/api-post-request.ts
@@ -1,9 +1,12 @@
 import axios, { AxiosResponse } from 'axios';
 
+// const urlBase = "http://127.0.0.1:8000/api/";
+const urlBase = "http://54.205.57.183:8000/api/";
+
 export const sendDataToBackend = async (formData: FormData): Promise<void> => {
     try {
         const response: AxiosResponse = await axios.post(
-            'http://127.0.0.1:8000/api/bandwidth-form/',
+            `${urlBase}bandwidth-form/`,
             formData
         );
         if (response.status !== 200) {
